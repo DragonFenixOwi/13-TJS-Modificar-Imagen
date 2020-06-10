c6ebd115d500f74f896bbdd6f5990a1209d85494
DragonFenixOwi <mecaowi@gmail.com>
2020-06-10 02:22:42 +0200

    Mejorando comentarios

--- js/Modificar_Imagen.js
+++ js/Modificar_Imagen.js
@@ -1,12 +1,41 @@
 
+/*
+    -----------------------------------------------------------
+        Autor: Osvaldo Aquino
+        nickname: owi
+        Hacer una imagen Dinamica
+    -----------------------------------------------------------
+*/
 
+
+/*
+    ------------------------------
+        VARIABLE GLOBALES
+    -----------------------------
+ */
+
+//Acceder a las etiquetas HTML
 var Stark_Texto = document.getElementById("titulo");
-
 var Stark_Imagen = document.getElementById("Elon_Musk")
 
+
+
+
+/*
+    ----------------
+        EVENTOS
+    ----------------
+ */
+
+
+
+// Evento que espera a que el usuario clickee sobre la imagen 
 Stark_Imagen.addEventListener("click",function()
     {
+        // Se altera el tamaño de la imagen. Aumentandola
         Stark_Imagen.style.width= "900px"; 
+
+        // Se cambia el nombre y el color de referencia de la imagen
         Stark_Texto.innerHTML="Stark Aumentado";
         Stark_Texto.style.color="purple";
 
@@ -14,7 +43,10 @@ Stark_Imagen.addEventListener("click",function()
 
         Stark_Imagen.addEventListener("click",function()
             {
+                // Se altera el tamaño de la imagen. Retornando a su tamaño inicial
                 Stark_Imagen.style.width= "600px"; 
+
+                // Se cambia el nombre y el color de referencia de la imagen
                 Stark_Texto.innerHTML="Stark Retroalimentado Inicial";
                 Stark_Texto.style.color="orange";
             });
